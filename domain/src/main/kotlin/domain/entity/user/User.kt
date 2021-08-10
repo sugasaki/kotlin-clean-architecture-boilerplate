466b13fddccfc0ea10c74785c0b75348cf39cf2c
sugasaki <sugasaki@gmail.com>
2021-08-10 12:34:38 +0900

    #6 Userクラス追加

---
+++ domain/src/main/kotlin/domain/entity/user/User.kt
@@ -0,0 +1,9 @@
+package domain.entity.user
+
+import domain.entity.IEntity
+
+data class User(
+    override val id: Int = -1,
+    val firstName: String,
+    val lastName: String,
+) : IEntity
---
+++ domain/src/main/kotlin/domain/repository/IUserRepository.kt
@@ -0,0 +1,7 @@
+package domain.repository
+
+import domain.entity.user.User
+
+interface IUserRepository {
+    suspend fun findById(id: String): User?
+}
